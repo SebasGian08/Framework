cb517b1916d14da35304de25412ef1adb3323fbb
GianSe08 <wongvasquezsebastian@gmail.com>
2023-07-24 16:42:19 -0500

    Actualizaciones Dia Lunes

--- Controllers/Clientes.php
+++ Controllers/Clientes.php
@@ -19,7 +19,7 @@ class Clientes extends Controllers{
 			header("Location:".base_url().'/dashboard');
 		}
 		$data['page_tag'] = "Clientes";
-		$data['page_title'] = "CLIENTES <small>Tienda Virtual</small>";
+		$data['page_title'] = "Clientes";
 		$data['page_name'] = "clientes";
 		$data['page_functions_js'] = "functions_clientes.js";
 		$this->views->getView($this,"clientes",$data);
